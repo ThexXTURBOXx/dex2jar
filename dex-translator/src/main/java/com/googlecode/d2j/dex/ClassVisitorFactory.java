072f82d29f5cc326ea97230f6d92a4ef911a4e8a
Col-E <mcoley2@gmu.edu>
2023-04-15 04:08:06 -0400

    Format docs on ClassVisitorFactory

--- dex-translator/src/main/java/com/googlecode/d2j/dex/ClassVisitorFactory.java
+++ dex-translator/src/main/java/com/googlecode/d2j/dex/ClassVisitorFactory.java
@@ -1,12 +1,17 @@
 package com.googlecode.d2j.dex;
 
 import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
 
 public interface ClassVisitorFactory {
-
     /**
-     * @param classInternalName class name
-     * @return a ClassVisitor, to generate validate .class file, ClassWriter.COMPUTE_MAXS is required for ClassWriter
+     * When implementing, if the visitor created is a {@link ClassWriter} you must use {@link ClassWriter#COMPUTE_MAXS}
+     * as the input flags if the class being made is <i>new</i> rather than a <i>modification</i> of an existing class.
+     *
+     * @param classInternalName
+     *         Internal class name,
+     *
+     * @return A visitor to generate class files.
      */
     ClassVisitor create(String classInternalName);
 

a0749588ba5af53d542199c085cfe7e629b64196
Nico Mexis <nico.mexis@kabelmail.de>
2024-10-25 08:06:32 +0200

    Update libraries and Gradle wrapper
    Also introduces Java 24 write support

--- d2j-base-cmd/src/main/java/com/googlecode/dex2jar/tools/Constants.java
+++ d2j-base-cmd/src/main/java/com/googlecode/dex2jar/tools/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
         throw new UnsupportedOperationException();
     }
 
-    public static final int MAX_JAVA_VERSION = 23;
+    public static final int MAX_JAVA_VERSION = 24;
 
     public static final int[] JAVA_VERSIONS = new int[]{
             0,
@@ -35,6 +35,7 @@ public final class Constants {
             Opcodes.V21,
             Opcodes.V22,
             Opcodes.V23,
+            Opcodes.V24,
     };
 
     public static final int ASM_VERSION = Opcodes.ASM9;
